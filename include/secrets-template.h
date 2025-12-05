41c599eb7fe7f5d398a33efd75edb07268b306ba
nito-008 <100199931+nito-008@users.noreply.github.com>
2025-12-05 15:37:12 +0900

    feat: mqtt connection

--- include/secrets-template.h
+++ include/secrets-template.h
@@ -1,11 +1,11 @@
 #pragma once
 
-inline constexpr char *WIFI_SSID = "YOUR_WIFI_SSID";
-inline constexpr char *WIFI_PASS = "YOUR_WIFI_PASSWORD";
+const char *WIFI_SSID = "YOUR_WIFI_SSID";
+const char *WIFI_PASS = "YOUR_WIFI_PASSWORD";
 
-inline constexpr char *MQTT_BROKER = "YOUR_MQTT_BROKER";
-inline constexpr char *MQTT_USERNAME = "YOUR_MQTT_USERNAME";
-inline constexpr char *MQTT_PASS = "YOUR_MQTT_PASSWORD";
+const char *MQTT_BROKER = "YOUR_MQTT_BROKER";
+const char *MQTT_USERNAME = "YOUR_MQTT_USERNAME";
+const char *MQTT_PASS = "YOUR_MQTT_PASSWORD";
 
-inline constexpr char *DB_API_URL = "YOUR_DB_API_URL";
-inline constexpr char *DB_API_KEY = "YOUR_DB_API_KEY";
+const char *DB_API_URL = "YOUR_DB_API_URL";
+const char *DB_API_KEY = "YOUR_DB_API_KEY";
